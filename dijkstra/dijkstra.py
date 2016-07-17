606ef0f6d4cc10a97782d9f09796a52d04edda5e
rlee287 <rlee287@yahoo.com>
2016-07-16 18:08:38 -0700

    Reworked dijkstra program to have to dicts

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -1,5 +1,7 @@
 import pprint
 import math
+from time import perf_counter
+import cProfile
 
 def find_neighbors(pos):
     row,col=pos
@@ -18,7 +20,7 @@ def find_distance(pos1,pos2):
 class DijkstraSolver:
     def __init__(self, filepath):
         """Zero based with
-           <space>=open
+           <space> or <period>=open
            s=start
            f=finish
            anything else=wall"""
@@ -26,8 +28,10 @@ class DijkstraSolver:
         # Map characters in file to initial distances or walls
         map_char={"s": 0,
                   "f": float("inf"),
-                  " ": float("inf")}
-        self.distmatrix=dict()
+                  " ": float("inf"),
+                  ".": float("inf")}
+        self.dist_visited=dict()
+        self.dist_unvisited=dict()
         # Read file contents, row by row
         with open(self.filepath,"r") as file_handle:
             has_start=False
@@ -50,9 +54,12 @@ class DijkstraSolver:
                             raise ValueError("File contains multiple start points")
                     init_dist=map_char.get(element,"wall")
                     visited=(init_dist=="wall" or element==has_start)
-                    self.distmatrix[(row, column)]={"dist": init_dist,
-                                                    "parent": None,
-                                                    "visited": visited}
+                    if visited:
+                        self.dist_visited[(row, column)]={"dist": init_dist,
+                                                         "parent": None}
+                    else:
+                        self.dist_unvisited[(row, column)]={"dist": init_dist,
+                                                            "parent": None}
                     column+=1
                 row+=1
             if not has_finish:
@@ -62,35 +69,33 @@ class DijkstraSolver:
 
     def find_path(self):
         current=self.start
-        current_point=self.distmatrix[self.start]
-        outside={'dist': 'wall', 'parent': None, 'visited': True}
-        while self.distmatrix[self.finish].get("visited") is False:
-            print("Currently on", current)
+        current_point=self.dist_unvisited[self.start]
+        outside={'dist': 'wall', 'parent': None}
+        while self.finish not in self.dist_visited:
+            #print("Currently on", current)
             for neighbor in find_neighbors(current):
-                print("  Checking neighbor",neighbor)
-                neighbor_point=self.distmatrix.get(neighbor,outside)
-                if not neighbor_point["visited"]:
-                    print("    Neighbor",neighbor,"is not visited")
-                    neighbor_dist=current_point["dist"]
-                    neighbor_dist+=find_distance(current,neighbor)
-                    if not isinstance((neighbor_point["dist"]), str) and \
-                                    neighbor_point["dist"]>neighbor_dist:
-                        neighbor_point["dist"]=neighbor_dist
-                        neighbor_point["parent"]=current
-                        print("    Neighbor distance is",neighbor_dist)
-                        print("    Neighbor parent is",current)
-                else:
-                    print("    Neighbor",neighbor,"is visited")
-            current_point["visited"]=True
-            print("  Point",current,"is visited")
-            print("  ",end="")
-            pprint.pprint(current_point)
+                #print("  Checking neighbor",neighbor)
+                neighbor_point=self.dist_unvisited.get(neighbor,outside)
+                #print("    Neighbor",neighbor,"is not visited")
+                neighbor_dist=current_point["dist"]
+                neighbor_dist+=find_distance(current,neighbor)
+                if not isinstance((neighbor_point["dist"]), str) and \
+                                neighbor_point["dist"]>neighbor_dist:
+                    neighbor_point["dist"]=neighbor_dist
+                    neighbor_point["parent"]=current
+                    #print("    Neighbor distance is",neighbor_dist)
+                    #print("    Neighbor parent is",current)
+                    #print("    Neighbor",neighbor,"is visited")
+            self.dist_visited[current]=self.dist_unvisited.pop(current)
+            #print("  Point",current,"is visited")
+            #print("  ",end="")
+            #pprint.pprint(current_point)
             mindist=float("inf")
             minpoint=None
-            for point in self.distmatrix:
-                point_dict=self.distmatrix[point]
+            for point in self.dist_unvisited:
+                point_dict=self.dist_unvisited[point]
                 point_dist=point_dict["dist"]
-                if point != current and not point_dict["visited"]:
+                if point != current:
                     if not isinstance(point_dist,str) and \
                                 mindist>point_dist:
                         mindist=point_dist
@@ -101,7 +106,7 @@ class DijkstraSolver:
             else:
                 exit_while=False
             current=minpoint
-            current_point=self.distmatrix[current]
+            current_point=self.dist_unvisited[current]
         #endwhile
         if exit_while:
             return "There is no pathway"
@@ -109,15 +114,23 @@ class DijkstraSolver:
             next_point=self.finish
             pathway=[next_point]
             while next_point != self.start:
-                next_point=self.distmatrix[next_point]["parent"]
+                next_point=self.dist_visited[next_point]["parent"]
                 pathway.append(next_point)
-            return pathway
+            pathway.reverse()
+            ans_dict=dict()
+            ans_dict["path"]=pathway
+            ans_dict["dist"]=self.dist_visited[self.finish]["dist"]
+            return ans_dict
 
 if __name__=="__main__":
-    filename=input("Please enter a filename:")
+    filename=input("Please enter a filename: ")
     q=DijkstraSolver(filename)
-    pprint.pprint(q.distmatrix)
+    pprint.pprint(q.dist_visited)
     print("Start node is:", q.start)
     print("Finish node is:", q.finish)
-    path=q.find_path()
-    print(path)
+    #start=perf_counter()
+    cProfile.run("path=q.find_path()")
+    #end=perf_counter()
+    print("Path is:")
+    pprint.pprint(path)
+    #print("Time taken is:",end-start)
