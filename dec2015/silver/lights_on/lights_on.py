f835a362216ce577d0e87c7a06e5a12cd32bdc49
rlee287 <rlee287@yahoo.com>
2016-08-11 13:07:43 -0700

    Write adjacent_rooms and continue developing lights_on

--- dec2015/silver/lights_on/lights_on.py
+++ dec2015/silver/lights_on/lights_on.py
@@ -1,3 +1,17 @@
+import pprint
+
+def adjacent_rooms(room,n):
+    x,y=room
+    list_adjacent=list()
+    if x>1:
+        list_adjacent.append((x-1,y))
+    if x<n:
+        list_adjacent.append((x+1,y))
+    if y>1:
+        list_adjacent.append((x,y-1))
+    if y<n:
+        list_adjacent.append((x,y+1))
+
 with open("lightson.in","r") as file_in:
     n,m=file_in.readline().split(" ")
     n,m=(int(n),int(m))
@@ -8,5 +22,15 @@ with open("lightson.in","r") as file_in:
         dict_rooms.setdefault((x1,y1),list())
         dict_rooms[(x1,y1)].append((sx,sy))
 
-import pprint
 pprint.pprint(dict_rooms)
+
+rooms_lit=set()
+visited_rooms=set()
+current_room=(1,1)
+rooms_lit.add(current_room)
+visited_rooms.add(current_room)
+first_time=True
+lit_unvisited=True
+while first_time:
+    for switch in dict_rooms[current_room]:
+        rooms_lit.add(switch)
