530d95e4fa13672595fb62d03539626e31205928
rlee287 <rlee287@yahoo.com>
2016-08-11 13:42:30 -0700

    Finish initial lights_on file

--- dec2015/silver/lights_on/lights_on.py
+++ dec2015/silver/lights_on/lights_on.py
@@ -2,15 +2,16 @@ import pprint
 
 def adjacent_rooms(room,n):
     x,y=room
-    list_adjacent=list()
+    list_adjacent=set()
     if x>1:
-        list_adjacent.append((x-1,y))
+        list_adjacent.add((x-1,y))
     if x<n:
-        list_adjacent.append((x+1,y))
+        list_adjacent.add((x+1,y))
     if y>1:
-        list_adjacent.append((x,y-1))
+        list_adjacent.add((x,y-1))
     if y<n:
-        list_adjacent.append((x,y+1))
+        list_adjacent.add((x,y+1))
+    return list_adjacent
 
 with open("lightson.in","r") as file_in:
     n,m=file_in.readline().split(" ")
@@ -21,6 +22,9 @@ with open("lightson.in","r") as file_in:
         x1,y1,sx,sy=(int(x1),int(y1),int(sx),int(sy))
         dict_rooms.setdefault((x1,y1),list())
         dict_rooms[(x1,y1)].append((sx,sy))
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            dict_rooms.setdefault((i,j),list())
 
 pprint.pprint(dict_rooms)
 
@@ -28,9 +32,18 @@ rooms_lit=set()
 visited_rooms=set()
 current_room=(1,1)
 rooms_lit.add(current_room)
-visited_rooms.add(current_room)
-first_time=True
 lit_unvisited=True
-while first_time:
+while lit_unvisited:
     for switch in dict_rooms[current_room]:
         rooms_lit.add(switch)
+        lit_unvisited_adjacent=rooms_lit & adjacent_rooms(current_room,n)
+        lit_unvisited_adjacent -= visited_rooms
+        lit_unvisited_set=rooms_lit-visited_rooms
+        lit_unvisited=(len(lit_unvisited_adjacent)>0)
+        visited_rooms.add(current_room)
+        if lit_unvisited:
+            current_room=list(lit_unvisited_adjacent)[0]
+
+pprint.pprint(rooms_lit)
+with open("lightson.out","w") as file_handle:
+    file_handle.write(str(len(rooms_lit))+"\n")
