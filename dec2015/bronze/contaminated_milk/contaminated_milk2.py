c890ffbd7cd0c71b1a69c9be0cb553df507cdf29
rlee287 <rlee287@yahoo.com>
2016-10-08 22:10:17 -0700

    Continue work on contaminated_milk

--- dec2015/bronze/contaminated_milk/contaminated_milk2.py
+++ dec2015/bronze/contaminated_milk/contaminated_milk2.py
@@ -1,24 +1,23 @@
-from collections import Counter
-
-set_possible=set()
-set_impossible=set()
-
 with open("badmilk.in", "r") as input_file:
     line_sizes=input_file.readline()
     n,m,d,s=tuple(line_sizes.split(" "))
     n,m,d,s=tuple([int(temp_var) for temp_var in [n,m,d,s]])
 
-    milk_count=Counter(list(range(1,m+1)))
+    milk_count=dict(zip(list(range(1,m+1)),[0]*m))
     person_drank=dict()
     person_sick=dict()
+    p_done=set()
     #print("line_drink")
     for record in range(d):
         line_drink=input_file.readline()
         #print(line_drink)
         p,milk,t=tuple(line_drink.split(" "))
         p,milk,t=tuple([int(temp_var) for temp_var in [p,milk,t]])
-        milk_count[p]+=1
+        if p not in p_done:
+            milk_count[milk]+=1
+            p_done|={p}
         person_drank[p]=person_drank.get(p,list())+[(milk,t)]
+    ##print(milk_count)
 
     #print("line_sick")
     for sick_record in range(s):
@@ -28,27 +27,31 @@ with open("badmilk.in", "r") as input_file:
         p,t=tuple(int(temp_var) for temp_var in [p,t])
         person_sick[p]=t
 
+set_possible=set(range(1,m+1))
+from pprint import pprint
+print("Initial possible")
+pprint(set_possible)
+pprint(person_drank)
+pprint(person_sick)
 for person,milk_record in person_drank.items():
-    nodrink_accumulate=set(range(1,m+1))
+    drank_before_sick=set()
     if person in person_sick.keys():
         time_sick=person_sick[person]
     else:
-        time_sick=-1
+        time_sick=-1 #sentinel value
+    print("Person",person)
     for milk,time in milk_record:
+        print(milk,time)
         if time<=time_sick:
-            set_possible|={milk}
-        else:
-            set_impossible|={milk}
-        nodrink_accumulate.remove(milk)
-    if time_sick==-1:
-        set_possible|=nodrink_accumulate
-    else:
-        set_impossible|=nodrink_accumulate
-print(set_possible)
-print(set_impossible)
-set_possible-=set_impossible
-print(milk_count)
-medicine=max([milk_count[p] for p in set_possible])
+            print('Drank {0} at time {1} before sick at {2}'.format(milk,time,time_sick))
+            drank_before_sick|={milk}
+        elif time_sick==-1:
+            drank_before_sick|=set(range(1,m+1))
+    set_possible&=drank_before_sick
+    print("Narrow down to")
+    pprint(set_possible)
+pprint(set_possible)
+medicine=max([milk_count[p] for p in set_possible]+[0])
 print(medicine)
 with open("badmilk.out","w") as output_file:
     output_file.write(str(medicine)+"\n")
